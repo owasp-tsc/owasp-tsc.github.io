0799f4876832eb235caca21bc5b8eaa981f7c7aa
Piyush <piyushnoida14@gmail.com>
2019-03-02 01:40:09 +0530

    timeline register

--- js/timeline.js
+++ js/timeline.js
@@ -13,7 +13,7 @@ $(document).ready(function($){
 	//on scolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
 		$timeline_block.each(function(){
-			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.50 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
 				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 			}
 		});
